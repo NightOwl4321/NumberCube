6666d40c569776d3c35d9aaf1efa5214b50b226f
Kendra Kleber <league@iMac.attlocal.net>
2016-03-02 18:29:29 -0800

    when you misspell roles
    you know life is going downhill

--- src/project/NumberCubeQuestion.java
+++ src/project/NumberCubeQuestion.java
@@ -1,16 +1,37 @@
 package project;
 
-
 public class NumberCubeQuestion {
 	public static void main(String[] args) {
+		
 	}
 
 	public static int[] getCubeTosses(NumberCube numberCube, int length) {
 		int[] values = new int[length];
-		for(int i = 0; i < length; i++) {
+		for (int i = 0; i < length; i++) {
 			values[i] = numberCube.toss();
 		}
 		return values;
+	}
+
+	public static int getLongestRun(int[] testValues) {
+		int longestIndex = -1;
+		int longestLength = 1;
+		int currentLength = 1;
+		int currentNum = testValues[0];
+		for (int i = 0; i < testValues.length; i++) {
+			if (currentNum == testValues[i]) {
+				currentLength++;
+				if (currentLength > longestLength) {
+					longestLength = currentLength;
+					longestIndex = i-currentLength+1;
+					
+				}
+			} else {
+				currentLength = 1;
+			}
+			currentNum = testValues[i];
+		}
+		return longestIndex;
 	}
 
 }
--- src/project/numberCubeTest.java
+++ src/project/numberCubeTest.java
@@ -1,6 +1,10 @@
 package project;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertArrayEquals;
+import static org.junit.Assert.assertEquals;
+
+import java.util.Arrays;
+import java.util.Random;
 
 import org.junit.Test;
 
@@ -12,15 +16,31 @@ public class numberCubeTest {
 		int[] testValues = { 3, 2, 5, 6, 1 };
 		numberCube.setValues(testValues);
 		assertArrayEquals(testValues, NumberCubeQuestion.getCubeTosses(numberCube, testValues.length));
+		
 	}
 
-	// @Test
-	// public void testPartB() throws Exception {
-	// int[] testValues = { 1, 5, 5, 2, 2, 2, 5, 5};
-	// assertEquals(3, NumberCubeQuestion.getLongestRun(testValues));
-	// int[] testValues2 = { 1, 5, 5, 4, 3, 1, 2, 2, 2, 2, 6, 1, 3, 3, 3, 5, 5, 5 };
-	// assertEquals(6, NumberCubeQuestion.getLongestRun(testValues2));
-	// }
+	 @Test
+	 public void testPartB() throws Exception {
+	 int[] testValues = { 1, 5, 5, 2, 2, 2, 5, 5};
+	 assertEquals(3, NumberCubeQuestion.getLongestRun(testValues));
+	 int[] testValues2 = {1, 5, 5, 4, 3, 1, 2, 2, 2, 2, 6, 1, 3, 3, 3, 5, 5, 5};
+	 assertEquals(6, NumberCubeQuestion.getLongestRun(testValues2));
+	 }
+	 
+	 @Test
+	 public void tosser() throws Exception {
+		 tosses();
+		 assertEquals(true, true);
+	 }
+
+	private static int[] tosses() {
+		int[] rolles = {0, 0, 0, 0, 0};
+		for(int i = 0; i < 5; i++) {
+			rolles[i] = new Random().nextInt(9);
+		}
+		System.out.println(Arrays.toString(rolles));
+		return rolles;
+	}
 
 }
 
@@ -31,6 +51,15 @@ class NumberCube {
 
 	public int toss() {
 		return values[testIndex++];
+	}
+	
+	public static int[] tosses() {
+		int[] rolles = {};
+		for(int i = 0; i < new Random().nextInt(10); i++) {
+			rolles[i] = new Random().nextInt(9);
+		}
+		System.out.println(rolles);
+		return rolles;
 	}
 
 	public void setValues(int[] testValues) {
